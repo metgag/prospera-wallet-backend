e4b05164a8be7e04295a3db109a3537286bdffbb
metgag <moreehigh1@gmail.com>
2025-09-25 13:50:49 +0700

    feat(users): get all users, except current logged in user

--- internals/handlers/user.handler.go
+++ internals/handlers/user.handler.go
@@ -1,6 +1,12 @@
 package handlers
 
-import "github.com/prospera/internals/repositories"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/prospera/internals/repositories"
+	"github.com/prospera/internals/utils"
+)
 
 type UserHandler struct {
 	ur *repositories.UserRepository
@@ -8,4 +14,20 @@ type UserHandler struct {
 
 func NewUserHandler(ur *repositories.UserRepository) *UserHandler {
 	return &UserHandler{ur: ur}
+}
+
+func (uh *UserHandler) HandlerGetUsers(ctx *gin.Context) {
+	uid, err := utils.GetUserIDFromJWT(ctx)
+	if err != nil {
+		return
+	}
+
+	users, err := uh.ur.GetUser(ctx.Request.Context(), uid)
+	if err != nil {
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"users": users,
+	})
 }
--- internals/repositories/user.repository.go
+++ internals/repositories/user.repository.go
@@ -1,6 +1,11 @@
 package repositories
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/prospera/internals/models"
+)
 
 type UserRepository struct {
 	db *pgxpool.Pool
@@ -8,4 +13,32 @@ type UserRepository struct {
 
 func NewUserRepository(db *pgxpool.Pool) *UserRepository {
 	return &UserRepository{db: db}
+}
+
+func (ur *UserRepository) GetUser(rctx context.Context, uid int) ([]models.User, error) {
+	sql := `
+		SELECT fullname
+		FROM profiles
+		WHERE id != $1
+	`
+
+	rows, err := ur.db.Query(rctx, sql, uid)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []models.User
+	for rows.Next() {
+		var user models.User
+		if err := rows.Scan(
+			&user.FullName,
+		); err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
 }
