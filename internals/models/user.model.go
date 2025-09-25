8ce704c0469a163f9dd0037ad801b59e30353319
metgag <moreehigh1@gmail.com>
2025-09-25 15:16:11 +0700

    feat(user): add user history transactions model

--- internals/models/user.model.go
+++ internals/models/user.model.go
@@ -3,3 +3,17 @@ package models
 type User struct {
 	FullName string `json:"full_name"`
 }
+
+type UserHistoryTransactions struct {
+	ID           int           `json:"id"`
+	Transactions []Transaction `json:"transactions"`
+}
+
+type Transaction struct {
+	ReceiverID      int    `json:"receiver_id"`
+	Avatar          string `json:"avatar"`
+	FullName        string `json:"receiver_name"`
+	PhoneNumber     string `json:"receiver_phone"`
+	TransactionType string `json:"transaction_type"`
+	Total           int    `json:"total"`
+}
