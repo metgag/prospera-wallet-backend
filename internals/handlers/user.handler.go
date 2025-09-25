94090a806827d2a4fd33c21c9a6a9314acd99ff8
metgag <moreehigh1@gmail.com>
2025-09-25 15:41:15 +0700

    feat(user): add error response and success response at get user Handler

--- internals/handlers/user.handler.go
+++ internals/handlers/user.handler.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/prospera/internals/models"
 	"github.com/prospera/internals/repositories"
 	"github.com/prospera/internals/utils"
 )
@@ -19,15 +21,20 @@ func NewUserHandler(ur *repositories.UserRepository) *UserHandler {
 func (uh *UserHandler) HandlerGetUsers(ctx *gin.Context) {
 	uid, err := utils.GetUserIDFromJWT(ctx)
 	if err != nil {
-		return
+		log.Println("get user token error: ", err)
+		utils.HandleError(ctx, http.StatusInternalServerError, "Internal Server Error", "unable to get user's token")
 	}
 
 	users, err := uh.ur.GetUser(ctx.Request.Context(), uid)
 	if err != nil {
+		log.Println("get user error: ", err)
+		utils.HandleError(ctx, http.StatusInternalServerError, "Internal Server Error", "unable get users")
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"users": users,
+	ctx.JSON(http.StatusOK, models.Response[[]models.User]{
+		Success: true,
+		Message: "User's list",
+		Data:    users,
 	})
 }
