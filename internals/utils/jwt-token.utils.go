8691d97ecb53ce29a350ef5be6dd51f8c8404a5c
Titus Rangga <titusrangga142@gmail.com>
2025-09-25 10:54:42 +0700

    feat: add features jwt token

---
+++ internals/utils/jwt-token.utils.go
@@ -0,0 +1,58 @@
+package utils
+
+import (
+	"errors"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/prospera/internals/pkg"
+)
+
+func GetUserIDFromJWT(ctx *gin.Context) (int, error) {
+	// Ambil header Authorization
+	authHeader := ctx.GetHeader("Authorization")
+	if authHeader == "" {
+		return 0, errors.New("missing token")
+	}
+
+	// Buang prefix "Bearer "
+	tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+
+	// Siapkan struct Claims
+	claims := &pkg.Claims{}
+
+	// Verify token
+	if err := claims.VerifyToken(tokenStr); err != nil {
+		return 0, err
+	}
+
+	// Ambil UserId
+	return claims.UserId, nil
+}
+
+func GetExpiredFromJWT(ctx *gin.Context) (time.Time, error) {
+	// Ambil header Authorization
+	authHeader := ctx.GetHeader("Authorization")
+	if authHeader == "" {
+		return time.Time{}, errors.New("missing token")
+	}
+
+	// Buang prefix "Bearer "
+	tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+
+	// Siapkan struct Claims
+	claims := &pkg.Claims{}
+
+	// Verify token
+	if err := claims.VerifyToken(tokenStr); err != nil {
+		return time.Time{}, err
+	}
+
+	// Ambil waktu expired
+	if claims.RegisteredClaims.ExpiresAt == nil {
+		return time.Time{}, errors.New("token has no expiry")
+	}
+
+	return claims.RegisteredClaims.ExpiresAt.Time, nil
+}
