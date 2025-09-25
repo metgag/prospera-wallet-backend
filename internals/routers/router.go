01ac9bb95c765f0b6e301f8d83f39ce980a1d1f9
metgag <moreehigh1@gmail.com>
2025-09-25 21:27:36 +0700

    feat(redis): add ping to redis client and init to router

--- internals/routers/router.go
+++ internals/routers/router.go
@@ -3,11 +3,13 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/prospera/internals/middlewares"
 	"github.com/redis/go-redis/v9"
 )
 
 func InitRouter(db *pgxpool.Pool, rdb *redis.Client) *gin.Engine {
 	router := gin.Default()
+	middlewares.InitRedis(rdb)
 
 	// Init Route Authentication
 	InitAuthRoutes(router, db)
