d65a79b0da3671ea5ca8c0100eaf51802a31ba1b
Titus Rangga <titusrangga142@gmail.com>
2025-09-25 13:23:56 +0700

    feat: add init redis

--- internals/routers/router.go
+++ internals/routers/router.go
@@ -3,9 +3,10 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 )
 
-func InitRouter(db *pgxpool.Pool) *gin.Engine {
+func InitRouter(db *pgxpool.Pool, rdb *redis.Client) *gin.Engine {
 	router := gin.Default()
 
 	return router
