0ba69f422a74b9554f217dc63fcb8c2a17dd28a6
metgag <moreehigh1@gmail.com>
2025-09-25 21:01:54 +0700

    refactor(transaction): mv transaction to user repository and user handler

--- internals/repositories/transaction.repository.go
+++ internals/repositories/transaction.repository.go
@@ -1,9 +1,6 @@
 package repositories
 
 import (
-	"context"
-	"errors"
-
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -13,23 +10,4 @@ type TransactionRepository struct {
 
 func NewTransactionRepository(db *pgxpool.Pool) *UserRepository {
 	return &UserRepository{db: db}
-}
-
-func (tr *TransactionRepository) SoftDeleteTransaction(rctx context.Context, transactionId int) error {
-	sql := `
-		UPDATE transactions
-		SET deleted_at = current_date
-		WHERE id = $1
-	`
-
-	ctag, err := tr.db.Exec(rctx, sql, transactionId)
-	if err != nil {
-		return err
-	}
-
-	if ctag.RowsAffected() == 0 {
-		return errors.New("no matching transaction id")
-	}
-
-	return nil
 }
