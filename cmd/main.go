cc422508b23e5739fcd3a82a412eb864184d55ce
metgag <moreehigh1@gmail.com>
2025-09-25 13:05:45 +0700

    feat: init router and init pg database

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/joho/godotenv"
+	"github.com/prospera/internals/configs"
+	"github.com/prospera/internals/routers"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db, err := configs.InitDB()
+	if err != nil {
+		log.Printf("DB ERROR: %s", err.Error())
+	}
+
+	if err := configs.PingDB(db); err != nil {
+		log.Printf("DB ERROR: %s", err.Error())
+	}
+	log.Println("pg connected")
+
+	router := routers.InitRouter(db)
+	router.Run(":8080")
 }
