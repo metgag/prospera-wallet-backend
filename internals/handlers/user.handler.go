964103c026e7c7d1804e24c77a1ef3f2ed73f5eb
radifan9 <radif.aghnadiin@gmail.com>
2025-09-26 08:58:40 +0700

    feat: add change password feature

--- internals/handlers/user.handler.go
+++ internals/handlers/user.handler.go
@@ -1,11 +1,13 @@
 package handlers
 
 import (
+	"errors"
 	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/prospera/internals/models"
+	"github.com/prospera/internals/pkg"
 	"github.com/prospera/internals/repositories"
 	"github.com/prospera/internals/utils"
 )
@@ -75,5 +77,60 @@ func (uh *UserHandler) HandleSoftDeleteTransaction(ctx *gin.Context) {
 		Success: true,
 		Message: "success",
 		Data:    "history deleted",
+	})
+}
+
+// Digunakan di halaman Change Password
+func (uh *UserHandler) ChangePassword(ctx *gin.Context) {
+	var req models.ChangePassword
+
+	if err := ctx.ShouldBind(&req); err != nil {
+		utils.HandleError(ctx, http.StatusBadRequest, "Bad Request", "failed to bind", err)
+		return
+	}
+
+	// Get ID from token
+	userID, err := utils.GetUserIDFromJWT(ctx)
+	if err != nil {
+		utils.HandleError(ctx, http.StatusUnauthorized, "Unauthorized", "user not found", err)
+		return
+	}
+
+	// Fetch current password hash from DB
+	oldPassDB, err := uh.ur.GetPasswordFromID(ctx, userID)
+	if err != nil {
+		utils.HandleError(ctx, http.StatusBadRequest, "bad request", "failed to fetch user credentials", err)
+		return
+	}
+
+	// Compare old password
+	hashCfg := pkg.NewHashConfig()
+	isMatched, err := hashCfg.ComparePasswordAndHash(req.OldPassword, oldPassDB)
+	if err != nil || !isMatched {
+		var handleErr error = err
+		if handleErr == nil && !isMatched {
+			handleErr = errors.New("old password does not match")
+		}
+		utils.HandleError(ctx, http.StatusBadRequest, "bad request", "old password does not match", handleErr)
+		return
+	}
+
+	// Hash new password
+	hashCfg.UseRecommended()
+	hashedPassword, err := hashCfg.GenHash(req.NewPassword)
+	if err != nil {
+		utils.HandleError(ctx, http.StatusInternalServerError, "internal server error", "failed to hash new password", err)
+		return
+	}
+
+	// Update password
+	if err := uh.ur.ChangePassword(ctx, userID, hashedPassword); err != nil {
+		utils.HandleError(ctx, http.StatusInternalServerError, "internal server error", "failed to change password", err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, models.Response[any]{
+		Success: true,
+		Message: "Change password successful",
 	})
 }
