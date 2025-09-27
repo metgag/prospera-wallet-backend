01cc9bdf23a221cfe134b5b8da93ea9eab46feb8
metgag <moreehigh1@gmail.com>
2025-09-27 18:44:54 +0700

    fix: remove omitempty at response model

--- internals/models/response.model.go
+++ internals/models/response.model.go
@@ -3,7 +3,7 @@ package models
 type Response[T any] struct {
 	Success bool   `json:"success" example:"true"`
 	Message string `json:"message" example:"Request processed successfully"`
-	Data    T      `json:"data,omitempty"`
+	Data    T      `json:"data"`
 }
 
 type ResponseLogin struct {
