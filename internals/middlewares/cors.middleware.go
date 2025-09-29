bc04b9ea94dc7e6c7c13fd909f4d21362f5cbcc6
radifan9 <radif.aghnadiin@gmail.com>
2025-09-29 08:55:42 +0700

    fix: change middleware cors

--- internals/middlewares/cors.middleware.go
+++ internals/middlewares/cors.middleware.go
@@ -10,13 +10,11 @@ import (
 func CORSMiddleware(ctx *gin.Context) {
 	// Daftar origin yang diizinkan
 	whitelist := []string{
-		"http://127.0.0.1:5500",
 		"http://localhost:5173",
 		"http://localhost",
 		"http://localhost:80",
-		"http://frontend",    // Add Docker service name
-		"http://frontend:80", // Add Docker service with port
-		"http://192.168.100.232",
+		"http://frontend",
+		"http://frontend:80",
 	}
 	origin := ctx.GetHeader("Origin")
 	if slices.Contains(whitelist, origin) {
@@ -28,7 +26,6 @@ func CORSMiddleware(ctx *gin.Context) {
 	// Header CORS standar
 	ctx.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
 	ctx.Header("Access-Control-Allow-Headers", "Authorization, Content-Type")
-	// ctx.Header("Access-Control-Allow-Credentials", "true")
 
 	// Jika request adalah preflight (OPTIONS)
 	if ctx.Request.Method == http.MethodOptions {
